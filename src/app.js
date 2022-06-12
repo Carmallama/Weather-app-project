0d9f4a1113cef6d60aeeac38ed0ee0a122f7799e
Carmallama <carmen.alger@hotmail.co.uk>
2022-06-12 12:03:43 +0000

    display temperature function added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+//change date and time
 let now = new Date();
 let currentDate = document.querySelector("#currentDate");
 let date = now.getDate();
@@ -29,3 +30,27 @@ let months = [
 ];
 let month = months[now.getMonth()];
 currentDate.innerHTML = `${day} ${date} ${month} ${hours}:${minutes}`;
+
+//show temperature
+
+let apiKey = "b6c769756646411c438f6a04abe73a94";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=reading&appid=${apiKey}&units=metric`;
+
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#currentTemperature");
+  temperatureElement.innerHTML = `${temperature}`;
+}
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
+
+//change city from search
+
+//get current position
+
+//change units of temperature
+
+//update weather overview data
+
+//change weather icon
