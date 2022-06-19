b4f6b5e14efb6f41819173e92c55d9ce19094100
Carmallama <carmen.alger@hotmail.co.uk>
2022-06-19 17:52:19 +0100

    rounding temperatures and added if statement to limit number of days being shown

--- src/app.js
+++ src/app.js
@@ -41,7 +41,15 @@ currentDate.innerHTML = `${day} ${date} ${month} ${hours}:${minutes}`;
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
   return days[day];
 }
@@ -53,10 +61,11 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
 
-  forecast.forEach(function (forecastDay) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index > 0 && index < 7) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col-2">
       <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
       <img src="https://openweathermap.org/img/wn/${
@@ -66,15 +75,16 @@ function displayForecast(response) {
       width="60"
       />
         <div class="weather-forecast-temperature">
-          <span class="weather-forecast-temperature-min">${
+          <span class="weather-forecast-temperature-min">${Math.round(
             forecastDay.temp.min
-          }</span> |
-          <span class="weather-forecast-temperature-max">${
+          )}°</span> |
+          <span class="weather-forecast-temperature-max">${Math.round(
             forecastDay.temp.max
-          }</span>
+          )}°</span>
         </div>
     </div>
           `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
