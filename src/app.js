90a5fe11d637ceab1b7002f1d44befe79a69e3d1
Carmallama <carmen.alger@hotmail.co.uk>
2022-06-19 17:25:03 +0100

    Called API to give lat and lon to send to the forecast function

--- src/app.js
+++ src/app.js
@@ -39,7 +39,8 @@ currentDate.innerHTML = `${day} ${date} ${month} ${hours}:${minutes}`;
 
 //display forecast
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -73,11 +74,18 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
-displayForecast();
 //show temperature
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+
+  let apiKey = "b6c769756646411c438f6a04abe73a94";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
-  console.log(response.data);
   let temperature = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector("#currentTemperature");
   let cityElement = document.querySelector("#city");
@@ -101,6 +109,8 @@ function displayTemperature(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
